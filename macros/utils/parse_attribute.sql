6681b4081d5df70ca22c1efec1396eb7d213ceb5
Bryce Codell <brycecodell2017@u.northwestern.edu>
2022-07-27 15:20:56 -0500

    add snowflake attr parsing

--- macros/utils/parse_attribute.sql
+++ macros/utils/parse_attribute.sql
@@ -15,3 +15,16 @@
 json_extract_path_text(attributes, '{{attribute_name}}'){{condition_str}}
 {%- endif -%}
 {%- endmacro -%}
+
+{%- macro snowflake__parse_attribute(attribute_name, condition) -%}
+{%- if condition is none -%}
+    {%- set condition_str = '' -%}
+{%- else -%}
+    {%- set condition_str = ' '~condition -%}
+{%- endif -%}
+{%- if attribute_name in ['event_id', 'event_at', var('customer_id')] -%}
+{{attribute_name}}{{condition_str}}
+{%- else -%}
+to_varchar(get_path(attributes, '{{attribute_name}}')){{condition_str}}
+{%- endif -%}
+{%- endmacro -%}
